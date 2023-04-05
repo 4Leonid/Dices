501376ac7acdb787e35c6503958bb450a9869e80
Leonid <leo-turko@mail.ru>
2023-04-05 16:11:39 +0300

    Make dice by the code

--- Dices/DiceViewController.swift
+++ Dices/DiceViewController.swift
@@ -12,10 +12,49 @@ import SnapKit
 class DiceViewController: UIViewController {
   
   //  MARK: - Private Properties
+  private let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+  
   private lazy var backgroundImage: UIImageView = {
     let element = UIImageView()
     element.image = UIImage(named: "GreenBackground")
     element.contentMode = .scaleAspectFill
+    return element
+  }()
+  
+  private lazy var logoImage: UIImageView = {
+    let element = UIImageView()
+    element.image = UIImage(named: "DiceeLogo")
+    element.contentMode = .scaleAspectFill
+    return element
+  }()
+  
+  private lazy var stackView: UIStackView = {
+    let element = UIStackView()
+    element.axis = .horizontal
+    element.spacing = 60
+    element.distribution = .fillEqually
+    return element
+  }()
+  
+  private lazy var diceOne: UIImageView = {
+    let element = UIImageView()
+    element.image = UIImage(named: "DiceFour")
+    element.contentMode = .scaleAspectFill
+    return element
+  }()
+  
+  private lazy var diceTwo: UIImageView = {
+    let element = UIImageView()
+    element.image = UIImage(named: "DiceFour")
+    element.contentMode = .scaleAspectFill
+    return element
+  }()
+  
+  private lazy var rollDice: UIButton = {
+    let element = UIButton(type: .system)
+    element.tintColor = .white
+    element.setTitle("Roll", for: .normal)
+    element.backgroundColor = #colorLiteral(red: 0.6068438888, green: 0.1102350578, blue: 0.1197155192, alpha: 1)
     return element
   }()
 
@@ -24,19 +63,41 @@ class DiceViewController: UIViewController {
     addViews()
     setConstraints()
   }
-
-
 }
 
 //  MARK: -  Private Methods
 extension DiceViewController {
   private func addViews() {
     view.addSubview(backgroundImage)
+    view.addSubview(logoImage)
+    view.addSubview(stackView)
+    stackView.addArrangedSubview(diceOne)
+    stackView.addArrangedSubview(diceTwo)
+    view.addSubview(rollDice)
   }
   
   private func setConstraints() {
     backgroundImage.snp.makeConstraints { make in
       make.edges.equalToSuperview()
+    }
+    
+    logoImage.snp.makeConstraints { make in
+      make.top.equalTo(120)
+      make.centerX.equalToSuperview()
+    }
+    
+    stackView.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+    }
+    
+    diceOne.snp.makeConstraints { make in
+      make.height.width.equalTo(120)
+    }
+    
+    rollDice.snp.makeConstraints { make in
+      make.centerX.equalToSuperview()
+      make.bottom.equalToSuperview().inset(120)
+      make.width.equalTo(diceOne.snp.width).dividedBy(2)
     }
   }
 }
